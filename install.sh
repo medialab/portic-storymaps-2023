f78fc56e3b6d6e802c020f33ee0976b41d2678ad
Robin de Mourat <robin.demourat@gmail.com>
2023-02-22 11:10:44 +0100

    try to fix prod bug with nbextensions

--- install.sh
+++ install.sh
@@ -13,11 +13,11 @@ pip install -e datalib
 pip install -r requirements.txt
 echo "ensuring notebook config is ok"
 pip install --upgrade notebook jupyter jupyterlab # need jupyter_client >= 4.2 for sys-prefix below
-jupyter nbextension install --sys-prefix --py vega  # not needed in notebook >= 5.3
-jupyter nbextension enable --py --sys-prefix ipyleaflet  # can be skipped for notebook 5.3 and above
+# jupyter nbextension install --sys-prefix --py vega  # not needed in notebook >= 5.3
+# jupyter nbextension enable --py --sys-prefix ipyleaflet  # can be skipped for notebook 5.3 and above
 
-jupyter nbextension install --py --sys-prefix keplergl # can be skipped for notebook 5.3 and above
-jupyter nbextension enable --py --sys-prefix keplergl # can be skipped for notebook 5.3 and above
+# jupyter nbextension install --py --sys-prefix keplergl # can be skipped for notebook 5.3 and above
+# jupyter nbextension enable --py --sys-prefix keplergl # can be skipped for notebook 5.3 and above
 
 echo "loading and preparing data"
 sh retrieve_data.sh
