7266a2c617508d9f418cdfad81be428470a40081
Robin de Mourat <robin.demourat@gmail.com>
2023-02-27 07:04:25 +0100

    fix small issue in retrieval script logs

--- retrieve_data.sh
+++ retrieve_data.sh
@@ -5,7 +5,7 @@ rm -f data/toflit18_bdd.zip
 mv "data/bdd courante.csv" "data/toflit18_all_flows.csv"
 echo "Download data : fetching latest navigo pointcalls data"
 
-echo "Download data : fetching latest navigo flows data"
+echo "Download data : fetching latest navigo pointcalls, flows and travels data"
 curl -o data/navigo_all_pointcalls.csv "http://sandbox.robindemourat.com/portic-datasprint-2023-data/pointcalls.csv"
 curl -o data/navigo_all_flows.csv "http://sandbox.robindemourat.com/portic-datasprint-2023-data/flows.csv"
 curl -o data/navigo_all_travels.csv "http://sandbox.robindemourat.com/portic-datasprint-2023-data/travels.csv"
@@ -14,7 +14,7 @@ curl -o data/navigo_all_travels.csv "http://sandbox.robindemourat.com/portic-dat
 # curl -o data/navigo_all_pointcalls.csv "data.portic.fr/api/pointcalls/?&format=csv"
 # curl -o data/navigo_all_pointcalls_1789.csv "data.portic.fr/api/pointcalls/?date=1789&format=csv"
 # curl -o data/navigo_all_pointcalls_1787.csv "data.portic.fr/api/pointcalls/?date=1787&format=csv"
-echo "Download data : fetching latest navigo flows data"
+echo "Download data : fetching latest navigo sources data"
 # curl -o data/navigo_all_flows.csv "data.portic.fr/api/rawflows/?format=csv"
 # curl -o data/navigo_all_flows_1789.csv "data.portic.fr/api/rawflows/?date=1789&format=csv"
 # curl -o data/navigo_all_flows_1787.csv "data.portic.fr/api/rawflows/?date=1787&format=csv"
