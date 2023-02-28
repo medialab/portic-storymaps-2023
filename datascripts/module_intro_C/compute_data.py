bca4f226abecf66c7e38f9a71c1ccb7936e54c41
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2023-02-28 12:44:59 +0100

    build data with imports/exports dropdown

--- datascripts/module_intro_C/compute_data.py
+++ datascripts/module_intro_C/compute_data.py
@@ -19,26 +19,32 @@ exchange_col = reader.headers["export_import"]
 value_col = reader.headers["value"]
 
 croissances = defaultdict(Counter)
+croissances_imports = defaultdict(Counter)
+croissances_exports = defaultdict(Counter)
 
 for row in reader:
     if row[year_col] in years and row[dirferme_col] in directions_fermes and row[source_col] == "1":
         croissances[row[dirferme_col]][row[year_col]] += float(row[value_col] or 0)
+        if row[exchange_col] == "Imports":
+            croissances_imports[row[dirferme_col]][row[year_col]] += float(row[value_col] or 0)
+        elif row[exchange_col] == "Exports":
+            croissances_exports[row[dirferme_col]][row[year_col]] += float(row[value_col] or 0)
 
 with open("evolution_directions_fermes.csv", "w") as f:
     writer = csv.writer(f)
-    writer.writerow(["year", "direction_ferme", "value"])
+    writer.writerow(["year", "direction_ferme", "total", "imports", "exports"])
     for year in years:
         for ferme in directions_fermes:
-            writer.writerow([year, ferme, croissances[ferme][year]])
+            writer.writerow([year, ferme, croissances[ferme][year], croissances_imports[ferme][year], croissances_exports[ferme][year]])
 
-def regress(region):
+def regress(region, growth, label):
     print()
-    print("Regression computation for", region)
+    print("Regression computation for", region, label)
     x_arr = []
     y_arr = []
-    for key in sorted(croissances[region].keys()):
+    for key in sorted(growth[region].keys()):
         x_arr.append(int(key))
-        y_arr.append(math.log(croissances[region][key]))
+        y_arr.append(math.log(growth[region][key]))
     print(x_arr, y_arr)
     x = np.array(x_arr).reshape((-1, 1))
     y = np.array(y_arr)
@@ -52,8 +58,10 @@ def regress(region):
 
 with open("regressions.csv", "w") as f:
     writer = csv.writer(f)
-    writer.writerow(["direction_ferme", "score_regression", "slope"])
+    writer.writerow(["direction_ferme", "score_regression_total", "slope_total", "score_regression_imports", "slope_imports", "score_regression_exports", "slope_exports"])
     for f in directions_fermes:
-        (score, slope) = regress(f)
-        writer.writerow([f, score, slope])
+        (score_t, slope_t) = regress(f, croissances, "total")
+        (score_i, slope_i) = regress(f, croissances_imports, "imports")
+        (score_e, slope_e) = regress(f, croissances_exports, "exports")
+        writer.writerow([f, score_t, slope_t, score_i, slope_i, score_e, slope_e])
 
