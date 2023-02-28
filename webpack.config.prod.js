b6d7fd76ddd030335546bb46442fb51b615bdca9
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2023-02-28 10:51:29 +0100

    oups

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -19,7 +19,7 @@ module.exports = {
             patterns: [
                 { from: "./public/data", to: "./data" },
                 { from: "./public/assets", to: "./assets" },
-                { from: "./public/thumbnails", to: "./thumbnails" }
+                { from: "./public/thumbnails", to: "./thumbnails" },
                 { from: "./datascripts/module_intro_A/map", to: "./assets/map-partner-groupings-balance-1789" }
             ],
         }),
