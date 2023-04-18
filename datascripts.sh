186a072b60e5de5fbf83740e13c29a1a3db09db8
Robin de Mourat <robin.demourat@gmail.com>
2023-04-18 09:32:30 +0200

    add datascripts sugar script

---
+++ datascripts.sh
@@ -0,0 +1,6 @@
+cd datascripts; 
+for f in *.py; 
+do if [ "$f" != "fetch_content.py" ] && [ "$f" != "build_data_subsets_for_marseille.py" ]; 
+then echo "execute python script $f";python "$f"; 
+fi; 
+done;
