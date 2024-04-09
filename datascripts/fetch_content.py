2b417d87edc5f6fb3dde01caac7330df95e2cabc
Robin de Mourat <robin.demourat@gmail.com>
2024-04-09 10:57:22 +0200

    fix issue with conclusion template close #22

--- datascripts/fetch_content.py
+++ datascripts/fetch_content.py
@@ -510,9 +510,11 @@ summary_js += "\nexport default [\n"
 for i, item in enumerate(summary):
   if i == 0:
     continue
+  print("item:")
+  print(item)
   base_str = json.dumps(item, indent = 2, ensure_ascii=False)[:-2]
   base_str += ",\n";
-  component = "ScrollyPage" if '"navGroup": "primary"' in base_str else "PlainPage"
+  component = "ScrollyPage" if '"navGroup": "primary"' in base_str and 'conclusion' not in base_str.lower() else "PlainPage"
   base_str += "  Component: " + component + ",\n";
   base_str += "  contents: {" + "\n"
   for lang in GDOC_URL.keys():
