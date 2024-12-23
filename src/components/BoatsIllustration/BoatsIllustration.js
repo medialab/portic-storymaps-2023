b4a01da68b0c47aa98bc8e8561710c243f743d3c
Robin de Mourat <robin.demourat@gmail.com>
2024-12-23 09:28:15 +0100

    improve flags animation

--- src/components/BoatsIllustration/BoatsIllustration.js
+++ src/components/BoatsIllustration/BoatsIllustration.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect, useMemo, useReducer } from "react";
 import { useSpring, animated } from '@react-spring/web';
-import {v4 as genId} from 'uuid';
-import {scaleLinear, scaleLog} from 'd3-scale';
-import {useInterval} from '../../utils/hooks';
+import { v4 as genId } from 'uuid';
+import { scaleLinear, scaleLog } from 'd3-scale';
+import { useInterval } from '../../utils/hooks';
 import { fixSvgDimension, polarToCartesian } from "../../utils/misc";
 import colorPalettes from '../../utils/colorPalettes';
 
@@ -28,19 +28,19 @@ const Hull = ({
 
   const colorScale = scaleLinear().domain([0, 12]).range(['darkgrey', 'black'])
   const colorModel = colorScale(Math.abs(12 - hourOfTime));
-  const {fill: color} = useSpring({
-    to: {fill: colorModel},
+  const { fill: color } = useSpring({
+    to: { fill: colorModel },
     config: { duration: hourDuration }
   });
   return (
     <>
-    <animated.path
-      className="hull"
-      style={{
-        fill: color
-      }}
-      d={`M 0 0 L ${width / 3} ${0} L ${width / 3} ${height / 4} L ${width * .6} ${height / 4} L ${width * .6} 0 L ${width} 0 L ${bottomRightX} ${height} L ${bottomLeftX} ${height} Z`}
-    />
+      <animated.path
+        className="hull"
+        style={{
+          fill: color
+        }}
+        d={`M 0 0 L ${width / 3} ${0} L ${width / 3} ${height / 4} L ${width * .6} ${height / 4} L ${width * .6} 0 L ${width} 0 L ${bottomRightX} ${height} L ${bottomLeftX} ${height} Z`}
+      />
     </>
   )
 
@@ -64,7 +64,7 @@ const Sail = ({
 }) => {
 
   const randomFactor = useMemo(() => Math.random() * .4 + 1, [])
-  
+
   const minRightX = width;
   const minLeftX = 0;
   const maxLeftX = 5 * distance;
@@ -77,7 +77,7 @@ const Sail = ({
     points: `0,${height * .1} ${width},${0} ${minRightX},${height / 2} ${width},${height * .9} 0,${height} ${minLeftX} ${height / 2}`
   }
 
-  const {points} = useSpring({
+  const { points } = useSpring({
     loop: true,
     to: [step1, step2, step1],
     from: step1,
@@ -85,8 +85,8 @@ const Sail = ({
   });
   const colorScale = scaleLinear().domain([0, 12]).range(['lightgrey', 'rgb(20,20,20)'])
   const colorModel = colorScale(Math.abs(12 - hourOfTime));
-  const {fill: color} = useSpring({
-    to: {fill: colorModel},
+  const { fill: color } = useSpring({
+    to: { fill: colorModel },
     config: { duration: hourDuration }
   });
 
@@ -130,8 +130,8 @@ const Matt = ({
   const secondWidth = width * .7;
   return (
     <>
-      <g transform={`translate(${width/2 - secondWidth/2}, ${height * .1})`}>
-        <Sail sailSpeed={sailSpeed} distance={distance} width={secondWidth} height={height /4} hourOfTime={hourOfTime} hourDuration={hourDuration} />
+      <g transform={`translate(${width / 2 - secondWidth / 2}, ${height * .1})`}>
+        <Sail sailSpeed={sailSpeed} distance={distance} width={secondWidth} height={height / 4} hourOfTime={hourOfTime} hourDuration={hourDuration} />
       </g>
       <g transform={`translate(0, ${height * .4})`}>
         <Sail sailSpeed={sailSpeed} distance={distance} width={width} height={height * .45} hourOfTime={hourOfTime} hourDuration={hourDuration} />
@@ -142,6 +142,50 @@ const Matt = ({
         y1={0}
         y2={height * 1.1}
       />
+    </>
+  )
+}
+
+const CampFire = ({
+  x,
+  y,
+  hourOfTime,
+  baseRadius
+}) => {
+  const fireSequence = useMemo(() => {
+    const numberOfSteps = 20;
+    const steps = [];
+    for (let i = 0; i < numberOfSteps; i++) {
+      const ratio = Math.random() + 1
+      steps.push({
+        r: baseRadius * ratio
+      })
+    }
+    return steps;
+  }, [])
+
+  const { r } = useSpring({
+    loop: true,
+    to: [{ r: baseRadius }, ...fireSequence, { r: baseRadius }],
+    from: { r: baseRadius },
+    config: { duration: 100 }
+  });
+  const id = useMemo(() => genId(), [])
+  return (
+    <>
+      <animated.circle
+        cx={x}
+        cy={y}
+        r={r}
+        fill={`url(#night-fire-${id})`}
+        opacity={hourOfTime >= 20 || hourOfTime <= 6 ? 1 : 0}
+      />
+      <radialGradient id={`night-fire-${id}`}>
+        {/* <animated.stop offset={'0%'} stopColor={'rgb(238, 237, 192)'} /> */}
+        <animated.stop offset={'0%'} stopOpacity={.5} stopColor={/*'rgb(255, 255, 255)'*/ 'red'} />
+        <animated.stop offset={'20%'} stopOpacity={1} stopColor={/*colorPalettes.ui.colorBackground*/ 'red'} />
+        <animated.stop offset={'80%'} stopOpacity={0} stopColor={/*colorPalettes.ui.colorBackground*/ 'orange'} />
+      </radialGradient>
     </>
   )
 }
@@ -179,32 +223,13 @@ const Boat = ({
   const step2 = {
     transform: `skewX(3)`,
   }
-  const {transform} = useSpring({
+  const { transform } = useSpring({
     loop: true,
     to: [step1, step2, step1],
     from: step1,
     config: { duration: 3000 }
   });
 
-  const fireSequence = useMemo(() => {
-    const numberOfSteps = 20;
-    const steps = [];
-    for (let i = 0 ; i < numberOfSteps ; i++) {
-      const ratio = Math.random() * 5 + 5
-      steps.push({
-        r: boatWidth / ratio
-      })
-    }
-    return steps;
-  }, [])
-
-  const {r} = useSpring({
-    loop: true,
-    to: [{r: boatWidth / 5},...fireSequence, {r: boatWidth / 5}],
-    from: {r: boatWidth / 5},
-    config: {duration: 100}
-
-  })
 
   useEffect(() => {
     const nOfMatts = Math.round(Math.random() * 2) + 1;
@@ -215,25 +240,46 @@ const Boat = ({
   const randomFactor = useMemo(() => Math.random(), [])
 
 
+  const campFires = useMemo(() => {
+    const numberOfFires = Math.round(1 + Math.random() * 10);
+    let f = [];
+    const hullHeight = height * hullHeightRatio;
+    for (let i = 0; i < numberOfFires; i++) {
+      const x = boatWidth * .1 + Math.random() * boatWidth * .8;
+      let y = height - hullHeight * .5;
+      if (x < boatWidth / 3 || x > boatWidth / 3 * 2) {
+        y = height - hullHeight;
+      }
+      f.push({
+        x,
+        y,
+        baseRadius: boatWidth / 30 + Math.random() * boatWidth / 20
+      })
+    }
+    return f;
+  }, [boatWidth, hullHeightRatio, height])
 
   return (
     <animated.g
       className="boat"
       transform={transform}
     >
-      <animated.circle
-        cx={boatWidth / 3}
-        cy={height - height * hullHeightRatio * .5}
-        r={r}
-        fill="url(#night-fire)"
-        opacity={hourOfTime >= 20 || hourOfTime <= 6 ? 1 : 0}
-      />
-      <radialGradient id={`night-fire`}>
-        {/* <animated.stop offset={'0%'} stopColor={'rgb(238, 237, 192)'} /> */}
-        <animated.stop offset={'0%'} stopOpacity={.5} stopColor={/*'rgb(255, 255, 255)'*/ 'red'} />
-        <animated.stop offset={'20%'} stopOpacity={1} stopColor={/*colorPalettes.ui.colorBackground*/ 'red'} />
-        <animated.stop offset={'80%'} stopOpacity={0} stopColor={/*colorPalettes.ui.colorBackground*/ 'orange'} />
-      </radialGradient>
+      {
+        campFires.map(({ x, baseRadius, y }, index) => (
+          <CampFire
+            key={index}
+            {
+            ...{
+              x,
+              y,
+              hourOfTime,
+              baseRadius
+            }
+            }
+          />
+        ))
+      }
+
       {
         matts.map((matt, mattIndex) => {
           const displace = (boatWidth * .9);
@@ -277,18 +323,18 @@ const Bird = ({
   containerHeight,
   distance
 }) => {
-  
+
   const dimension = containerHeight / 60 * (distance / 2);
   const step1 = {
-    points: `0,${0}, ${dimension/2},${dimension/3} ${dimension},${0} ${dimension/2} ${dimension / 2}`
+    points: `0,${0}, ${dimension / 2},${dimension / 3} ${dimension},${0} ${dimension / 2} ${dimension / 2}`
   }
   const step2 = {
-    points: `0,${dimension}, ${dimension/2},${0} ${dimension},${dimension} ${dimension/2} ${dimension / 3}`
+    points: `0,${dimension}, ${dimension / 2},${0} ${dimension},${dimension} ${dimension / 2} ${dimension / 3}`
   }
 
   const moveSteps = useMemo(() => {
     const steps = []
-    for (let i = 0 ; i < 30 ; i++) {
+    for (let i = 0; i < 30; i++) {
       const y = Math.random() * boatHeight * 1.3 + containerHeight - boatHeight * 1.5;
       const x = Math.random() * boatHeight * 2 - boatHeight;
       const scale = Math.random() + .5
@@ -299,13 +345,13 @@ const Bird = ({
     return steps;
   }, [boatHeight, containerHeight])
 
-  const {points} = useSpring({
+  const { points } = useSpring({
     loop: true,
     to: [step1, step2, step1],
     from: step1,
     config: { duration: 300 }
   });
-  const {transform} = useSpring({
+  const { transform } = useSpring({
     loop: true,
     to: [...moveSteps, moveSteps[0]],
     from: moveSteps[0],
@@ -344,7 +390,7 @@ const MovingBoat = ({
     if (distance > .5 && Math.random() > .5) {
       const numberOfBirds = 1 + Math.round(Math.random() * 20);
       let b = [];
-      for (let i = 0 ; i < numberOfBirds ; i++) {
+      for (let i = 0; i < numberOfBirds; i++) {
         b.push(i);
       }
       return b;
@@ -352,7 +398,7 @@ const MovingBoat = ({
       return [];
     }
   }, [distance])
-  const start = -containerWidth * 0.3; 
+  const start = -containerWidth * 0.3;
   const end = containerWidth * 1.3;
   const step1 = {
     transform: `translate(${rightToLeft ? end : start}, 0)`
@@ -361,10 +407,10 @@ const MovingBoat = ({
     transform: `translate(${rightToLeft ? start : end}, 0)`
   }
   if (startAt) {
-    step1.transform =  `translate(${startAt}, 0)`
+    step1.transform = `translate(${startAt}, 0)`
   }
   const randomFactor = useMemo(() => Math.random(), [])
-  const {transform} = useSpring({
+  const { transform } = useSpring({
     from: step1,
     to: step2,
     config: { duration: PASS_DURATION * (2 - distance * (1 + randomFactor / 10)) }
@@ -374,7 +420,7 @@ const MovingBoat = ({
     <animated.g
       transform={transform}
     >
-      <g style={{opacity: distance}} transform={rightToLeft ? 'scale(-1, 1)' : ''}>
+      <g style={{ opacity: distance }} transform={rightToLeft ? 'scale(-1, 1)' : ''}>
         <g transform={`translate(0, ${containerHeight - height})`}>
           <Boat
             x={0}
@@ -394,7 +440,7 @@ const MovingBoat = ({
             />
           ))
         }
-        
+
       </g>
     </animated.g>
   )
@@ -416,14 +462,14 @@ const BoatsIllustration = ({
   const width = fixSvgDimension(inputWidth);
   const height = fixSvgDimension(inputHeight);
   const createBoat = (payload = {}) => {
-    const {startAt = 0} = payload;
+    const { startAt = 0 } = payload;
     const id = genId();
     const distance = Math.random() * .8 + .2;
     const rightToLeft = Math.random() >= .5;
     const size = Math.round(Math.random() * 3) + 1;
     return {
       id,
-      distance, 
+      distance,
       rightToLeft,
       size,
       createdAt: new Date().getTime(),
@@ -431,7 +477,7 @@ const BoatsIllustration = ({
     }
   }
   const [boats, updateBoats] = useReducer((b, action) => {
-    switch(action.type) {
+    switch (action.type) {
       case 'removeBoat':
         return Object.entries(b).reduce((res, [id, boat]) => {
           if (id !== action.payload) {
@@ -457,7 +503,7 @@ const BoatsIllustration = ({
     // add boats regularly
     const timeout = setTimeout(() => {
       // add boat
-      updateBoats({type: 'createBoat'})
+      updateBoats({ type: 'createBoat' })
     }, PASS_DURATION / 10)
     return () => clearTimeout(timeout)
   }, [boats, updateBoats])
@@ -471,15 +517,15 @@ const BoatsIllustration = ({
           [id]: boat
         }
       } else {
-        updateBoats({type: 'removeBoat', payload: id})
+        updateBoats({ type: 'removeBoat', payload: id })
         return res;
       }
     }, {})
   }, UPDATE_RATE)
 
   useEffect(() => {
-    for (let i = 0 ; i < 5 ; i++) {
-      updateBoats({type: 'createBoat', payload: {startAt: width * Math.random()}})
+    for (let i = 0; i < 5; i++) {
+      updateBoats({ type: 'createBoat', payload: { startAt: width * Math.random() } })
     }
   }, []);/* eslint react-hooks/exhaustive-deps : 0 */
 
@@ -487,14 +533,14 @@ const BoatsIllustration = ({
   const sunAngleInDeg = 360 / 24 * hourOfTime + 90;
   const sunAngle = sunAngleInDeg * (Math.PI / 180);
   const [sunX, sunY] = polarToCartesian(sunRadius, sunAngle);
-  const sunTransformModel = `translate(${sunX + width/2},${sunY + height})`;
-  const {transform: sunTransform} = useSpring({
-    to: {transform: sunTransformModel},
+  const sunTransformModel = `translate(${sunX + width / 2},${sunY + height})`;
+  const { transform: sunTransform } = useSpring({
+    to: { transform: sunTransformModel },
     config: { duration: hourDuration }
   });
   // const lumScale = scaleLinear().domain([12, 0]).range(['#336d7c', '#ebebeb']);
   // const lumScale = scaleLinear().domain([12, 0]).range(['#340b0b', '#ebebeb']);
-  const lumScale = scaleLinear().domain([12, 0]).range(['#181D31', '#678983','#678983', '#340b0b' ]);
+  const lumScale = scaleLinear().domain([12, 0]).range(['#181D31', '#678983', '#678983', '#340b0b']);
   const darkRadiusScale = scaleLinear().domain([12, 0]).range([30, 50])
   const bgColorModel = lumScale(Math.abs(hourOfTime - 12))
   const darkRadiusModel = darkRadiusScale(Math.abs(hourOfTime - 12)) + '%';
@@ -527,18 +573,18 @@ const BoatsIllustration = ({
       </animated.g>
       {
         Object.entries(boats).map(([id, {
-          distance, 
-          rightToLeft, 
+          distance,
+          rightToLeft,
           size,
           startAt
         }]) => (
-          <MovingBoat 
-            key={id} 
-            distance={distance} 
-            size={size} 
-            rightToLeft={rightToLeft} 
-            containerHeight={height} 
-            containerWidth={width} 
+          <MovingBoat
+            key={id}
+            distance={distance}
+            size={size}
+            rightToLeft={rightToLeft}
+            containerHeight={height}
+            containerWidth={width}
             startAt={startAt}
             hourOfTime={hourOfTime}
             hourDuration={hourDuration}
